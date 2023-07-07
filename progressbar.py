12f1340326778ae850a6ad8a295440f257fa8bd8
Quinn Henry <78760148+TheQuib@users.noreply.github.com>
2023-07-07 12:46:26 -0400

    ✨ Fix progress bar drawing

--- progressbar.py
+++ progressbar.py
@@ -20,19 +20,24 @@ __location__ = os.path.realpath(
 picdir = __location__ + "/assets"
 logging.basicConfig(level=logging.DEBUG)
 
-def progress_bar(x, y, width, height, progress, bg=(129,66,97), fg=(211,211,211), fg2=(15,15,15)):
-    Himage = Image.new('1', (width, height), 255)
-    draw = ImageDraw.Draw(Himage)
-    # Draw the background
-    draw.rectangle((x+(height/2), y, x+width+(height/2), y+height))#, fill=fg2, width=10)
-    draw.ellipse((x+width, y, x+height+width, y+height))#, fill=fg2)
-    draw.ellipse((x, y, x+height, y+height))#, fill=fg2)
-    width = int(width*progress)
-    # Draw the part of the progress bar that is actually filled
-    draw.rectangle((x+(height/2), y, x+width+(height/2), y+height))#, fill=fg, width=10)
-    draw.ellipse((x+width, y, x+height+width, y+height))#, fill=fg)
-    draw.ellipse((x, y, x+height, y+height))#, fill=fg)
-    return(Himage)
+def draw_progress_bar(x, y, image_width, image_height, bar_width, bar_height, progress, padding, background_color, bar_fill_color):
+    # Create a new image with the given image_width and image_height
+    image = Image.new('RGB', (image_width, image_height), (255,255,255))
+    draw = ImageDraw.Draw(image)
+
+    # Calculate the width of the progress bar based on the progress value
+    filled_width = int((progress / 100) * (bar_width - 2 * padding))
+
+    # Draw the background of the progress bar
+    background_coords = [(x + padding, y + padding), (x + bar_width - padding - 1, y + bar_height - padding - 1)]
+    draw.rounded_rectangle(background_coords, radius=(bar_height - 2 * padding) // 2, fill=background_color)
+
+    # Draw the filled portion of the progress bar
+    filled_coords = [(x + padding, y + padding), (x + filled_width + padding, y + bar_height - padding - 1)]
+    draw.rounded_rectangle(filled_coords, radius=(bar_height - 2 * padding) // 2, fill=bar_fill_color)
+
+    # Show the progress bar
+    return(image)
 
 try:
     logging.info("epd7in5_V2 Test")
@@ -43,7 +48,7 @@ try:
     epd.Clear()
 
     logging.info("Draw progress bar")
-    bar = progress_bar(10, 10, epd.width, epd.height, 0.25)
+    bar = drawnImage = draw_progress_bar(50, 50, epd.width, epd.height, 300, 40, 75, 4, (50, 50, 50), (150, 150, 150))
     epd.display(epd.getbuffer(bar))
 
     logging.info("Set display to sleep")

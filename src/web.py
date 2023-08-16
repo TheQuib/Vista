32db0413856c7e9724d3a43edd94eef21fad7a9f
Quinn Henry <19qhenry@gmail.com>
2023-08-15 22:52:28 -0400

    🏋️ Heavy web server overhaul

--- src/web.py
+++ src/web.py
@@ -1,17 +1,35 @@
 from flask import Flask, render_template, request
+import logging
+import os
+
+logging.basicConfig(level=logging.DEBUG)
 
 class Webserver:
-    def start_server():
-        app = Flask(__name__, template_folder='web/templates', static_folder='web/static')
+    def __init__(self):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        template_dir = os.path.join(current_directory, 'web', 'templates')
+        static_dir = os.path.join(current_directory, 'web', 'static')
+        self.app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
+    def start_server(self, planTotal, planRemaining, bonusTotal, bonusRemaining):
+        planTotal = float(planTotal)
+        planRemaining = float(planRemaining)
+        planUsed = planTotal - planRemaining
+        planUsed = round(planUsed, 2)
+        planPercentageRemaining = round((planRemaining / planTotal) * 100, 2)  # Calculating percentage
 
-        @app.route('/')
-        def home():
-            return render_template('home.html')
+        bonusTotal = float(bonusTotal)
+        bonusRemaining = float(bonusRemaining)
+        bonusUsed = bonusTotal - bonusRemaining
+        bonusUsed = round(bonusUsed, 2)
+        bonusPercentageRemaining = round((bonusRemaining / bonusTotal) * 100, 2)  # Calculating percentage
 
-        @app.route('/settings')
+        @self.app.route('/')
+        def home():            
+            return render_template('home.html', regular_total=planTotal, regular_used=planUsed, regular_percentage=planPercentageRemaining, bonus_total=bonusTotal, bonus_used=bonusUsed, bonus_percentage=bonusPercentageRemaining)
+
+        @self.app.route('/settings')
         def settings():
             return render_template('settings.html')
 
-        if __name__ == '__main__':
-            app.run(debug=True)
+        self.app.run()

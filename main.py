5dc6c76b1ff8bef1b94591d7ec2005c6414f7754
Quinn Henry <19qhenry@gmail.com>
2023-08-15 22:54:21 -0400

    🌐 Start web server in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 from src.scraper import GetHTML
-from src.progressbar import ProgressBar
+#from src.progressbar import ProgressBar
 from src.web import Webserver
 import os, logging, time
 
@@ -12,13 +12,17 @@ def main(example):
         with open('example.html') as html:
             logging.info("Parsing example HTML to get necessary values")
             hughesnet_values = GetHTML.parse_website(html)
-            print(hughesnet_values)
+            #print(hughesnet_values)
+
     else:
         logging.info("Load HTML from website")
         html = GetHTML.load_website
         logging.info("Parse HTML to get necessary values")
         hughesnet_values = GetHTML.parse_website(html)
-        print(hughesnet_values)
+        #print(hughesnet_values)
+    
+    web = Webserver()
+    web.start_server(hughesnet_values['planTotal'], hughesnet_values['planRemaining'], hughesnet_values['bonusTotal'], hughesnet_values['bonusRemaining'])
 
 
 main(useExampleHtml)

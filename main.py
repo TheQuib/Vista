94d44e1b48576a01a0578d7bd784943b3fbfb705
Quinn Henry <19qhenry@gmail.com>
2024-03-28 14:37:53 -0400

    Remove web server, convert comments to logs

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ __location__ = os.path.realpath(
     )
 )
 
-useExampleHtml = True
-
 def main(example):
     if example == True:
         with open('example.html') as html:
@@ -31,8 +29,8 @@ def main(example):
         logging.info("Parse HTML to get necessary values")
         hughesnet_values = GetHTML.parse_website(html)
         #print(hughesnet_values)
-   
-    # Calculate percentages
+
+    logging.info("Calculating usage percentages")
     planRemaining = float(hughesnet_values['planRemaining'])
     planTotal = float(hughesnet_values['planTotal'])
     planPercentRemaining = round((planRemaining / planTotal) * 100, 2)
@@ -40,40 +38,34 @@ def main(example):
     bonusTotal = float(hughesnet_values['bonusTotal'])
     bonusPercentRemaining = round((bonusRemaining / bonusTotal) * 100, 2)
 
-    # Get IP Address of device
+    logging.info("Getting IP Address of device")
     address = psutil.net_if_addrs()
     local_ip = address['wlan0'][0].address
 
-    # Display progress bar on display
+    logging.info("Creating DisplayManager() object")
     dm = DisplayManager()
+
+    logging.info("Drawing static items")
     dm.draw_text("SkyStat", 70, 15, 40)
     dm.draw_text("github.com/TheQuib/SkyStat", 70, 58, 17)
     dm.draw_text("Manage this display...", 495, 445, 17)
     dm.draw_qr_code(655, 375, 100, local_ip)
     dm.draw_line((0,90), (800, 90), width=3)
 
+    logging.info("Drawing regular plan itmes")
     dm.draw_box((70,104), (400,220), 5)
     dm.draw_text("Regular Plan", 80, 114, 28, __location__ + '/src/font/Asap/static/Asap-SemiBold.ttf')
     dm.draw_progress_bar(planPercentRemaining, 80, 148, 300, 40)
     dm.draw_text(str(planPercentRemaining) + " remaining", 80, 188, 28)
 
+    logging.info("Drawing bonus plan items")
     dm.draw_box((70,282), (400,398), 5)
     dm.draw_text("Bonus Plan", 80, 292, 28, __location__ + '/src/font/Asap/static/Asap-SemiBold.ttf')
     dm.draw_progress_bar(bonusPercentRemaining, 80, 326, 300, 40)
     dm.draw_text(str(bonusPercentRemaining) + " remaining", 80, 366, 28)
 
+    logging.info("Pushing image to E-Paper")
     dm.display_image()
 
-    # Start web server process
-    web = Webserver(clear_display=ScreenControls.clearScreen())
-    web_thread = Thread(target=web.start_server, args=(hughesnet_values['planTotal'], hughesnet_values['planRemaining'], hughesnet_values['bonusTotal'], hughesnet_values['bonusRemaining']))
-    web_thread.daemon = True
-    web_thread.start()
-
-    # Wait 1 minute (dev purposes)
-    time.sleep(60)
-    web.stop_server()
-    web_thread.join()
-
-
-main(useExampleHtml)
+logging.info("Running main")
+main(eample=True)

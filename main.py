830c2ab88eb7e6c81a593b8da698834a7097708d
Quinn Henry <19qhenry@gmail.com>
2024-03-29 15:54:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 import drawImage
 
-def main():
-    values = drawImage.GatherValues(debug=True)
+def main(debug):
+    values = drawImage.GatherValues(debug)
 
     drawImage.DrawImage(values.todays_date, values.lastUpdated_dateTime, values.planPercentRemaining, values.bonusPercentRemaining, values.random_fact, values.local_ip)
 
 if __name__ == "__main__":
-    main()
+    main(debug=True)

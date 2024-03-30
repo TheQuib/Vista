32f3b8c3f0f36d54992fd346b8c3d0a442d2c9b5
Quinn Henry <19qhenry@gmail.com>
2024-03-30 10:38:07 -0400

    Utilize function

--- boot.py
+++ boot.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 
+import time
 import drawImage
 
-drawImage.BootScreen()
+def boot(debug):
+    values = drawImage.GatherValues(debug)
+
+    drawImage.BootScreen()
+
+    time.sleep(120)
+
+    drawImage.DrawImage(values.todays_date, values.lastUpdated_dateTime, values.planPercentRemaining, values.bonusPercentRemaining, values.random_fact, values.web_address)
+
+boot(debug=True)

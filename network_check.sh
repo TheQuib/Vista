9cbd745d12cd4930b0e715b55475ac37d55b6969
Quinn Henry <19qhenry@gmail.com>
2024-03-29 21:51:42 -0400

    Add network check script

---
+++ network_check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Initialize variable to hold the gateway IP
+GATEWAY_IP=""
+
+# Function to get the default gateway IP
+get_gateway_ip() {
+    GATEWAY_IP=$(ip route | grep default | awk '{print $3}')
+}
+
+# First attempt to get the gateway IP
+get_gateway_ip
+
+# Wait until a default gateway is set
+while [ -z "$GATEWAY_IP" ]; do
+    echo "No default gateway found. Waiting for network connection..."
+    sleep 5
+    get_gateway_ip  # Try to get the gateway IP again
+done
+
+echo "Default gateway found: $GATEWAY_IP. Checking network connectivity..."
+
+# Now, wait for the network to become available by pinging the gateway
+while ! ping -c 1 -W 1 $GATEWAY_IP > /dev/null; do
+    echo "Waiting for network to become available..."
+    sleep 5
+done
+
+echo "Network is up. Executing main.py."
+/usr/bin/python3 /opt/skystat/main.py >> /opt/skystat/main.log 2>&1
